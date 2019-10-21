a3413d8e2ccad2e87ea2ddeab1891abbe2d8a310
BradleyTim <timbradley94@gmail.com>
2019-10-21 12:59:40 +0300

    add movie-detail route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from "./app.component";
 import { NHeaderComponent } from "./n-header/n-header.component";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "movies", pathMatch: "full" },
+  { path: "movies", component: HomeComponent },
+  { path: "movies/:id", component: MovieDetailComponent },
   { path: "about", component: AboutComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, NHeaderComponent, AboutComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NHeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    MovieDetailComponent
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movie-detail/movie-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IMovie } from "../shared/movie";
+import { MovieService } from "../home/movie.service";
+
+@Component({
+  selector: "app-movie-detail",
+  templateUrl: "./movie-detail.component.html",
+  styleUrls: ["./movie-detail.component.css"],
+  providers: [MovieService]
+})
+export class MovieDetailComponent implements OnInit {
+  movie: IMovie;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.movieService.getMovies().filter(movie => {
+      movie.movieId == id ? (this.movie = movie) : "";
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(["/movies"]);
+  }
+}
